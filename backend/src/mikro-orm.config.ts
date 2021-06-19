c80544bc738d919c6da4f539d858d54fd24aa842
SteveMelons <oli.olioli.fischer@gmail.com>
2021-06-19 15:43:58 +0200

    changed some constants to env variables

--- backend/src/mikro-orm.config.ts
+++ backend/src/mikro-orm.config.ts
@@ -1,12 +1,12 @@
 import { MikroORM } from "@mikro-orm/core";
-import { DB_NAME, __prod__ } from "./constants";
+import { __prod__ } from "./constants";
 import { User } from "./entities/User";
 
 export default {
   entities: [User],
-  dbName: DB_NAME,
+  dbName: process.env.DB_NAME,
   type: "mongo",
-  clientUrl: "mongodb://mongo:27017",
+  clientUrl: process.env.MONGO_URL,
   debug: !__prod__,
   ensureIndexes: true,
 } as Parameters<typeof MikroORM.init>[0];
--- backend/src/types/env.d.ts
+++ backend/src/types/env.d.ts
@@ -2,7 +2,8 @@ declare namespace NodeJS {
   interface ProcessEnv {
     PORT: string;
     SESSION_SECRET: string;
-    SESSION_COOKIE_NAME: string;
+    MONGO_URL: string;
     REDIS_URL: string;
+    DB_NAME: string;
   }
 }
