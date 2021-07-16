b11a27afa8b8b0efdf215156572bd6546611c1e8
SteveMelons <oli.olioli.fischer@gmail.com>
2021-07-16 19:45:55 +0200

    setup for prod

--- backend/src/index.ts
+++ backend/src/index.ts
@@ -69,6 +69,7 @@ import { IOSession } from "./types/session";
     secret: process.env.SESSION_SECRET!,
     resave: false,
     saveUninitialized: false,
+    proxy: true,
     cookie: {
       maxAge: 1000 * 60 * 60 * 24 * 365, // 1 year
       httpOnly: true,
