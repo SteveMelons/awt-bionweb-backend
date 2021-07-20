c49a34acab96a7d10d985e146afbd28264e8be6f
SteveMelons <oli.olioli.fischer@gmail.com>
2021-07-20 23:36:40 +0200

    added dynamic resizing of carousel

--- frontend/src/components/StudectCarousel.tsx
+++ frontend/src/components/StudectCarousel.tsx
@@ -1,10 +1,12 @@
 import { Box, useTheme } from "@material-ui/core";
-import React from "react";
+import React, { createRef, useState } from "react";
 import { Carousel } from "react-responsive-carousel";
 import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
 import { User } from "../types/User";
 import StudectCard from "./StudectCard";
 import "../StudectCarousel.css";
+import { mapNumberRange } from "../utils/mapNumberRange";
+import { useEffect } from "react";
 
 interface StudectCarouselProps {
   users: User[];
@@ -13,8 +15,39 @@ interface StudectCarouselProps {
 const StudectCarousel: React.FC<StudectCarouselProps> = ({ users }) => {
   const theme = useTheme();
 
+  const [sliderState, setSliderState] = useState(80);
+
+  const ref = createRef<HTMLElement>();
+
+  useEffect(() => {
+    if (ref?.current?.offsetWidth) {
+      const number = mapNumberRange(
+        ref?.current?.offsetWidth,
+        2560,
+        320,
+        10,
+        90
+      );
+      setSliderState(number);
+    }
+  }, []);
+
+  window.addEventListener("resize", () => {
+    if (ref?.current?.offsetWidth) {
+      const number = mapNumberRange(
+        ref?.current?.offsetWidth,
+        2560,
+        320,
+        10,
+        90
+      );
+      setSliderState(number);
+    }
+  });
+
   return (
     <Box
+      ref={ref}
       sx={{
         flexGrow: 1,
         marginX: theme.spacing(6),
@@ -25,7 +58,7 @@ const StudectCarousel: React.FC<StudectCarouselProps> = ({ users }) => {
       <Carousel
         autoPlay
         centerMode
-        centerSlidePercentage={40}
+        centerSlidePercentage={sliderState}
         showArrows
         infiniteLoop
         emulateTouch
